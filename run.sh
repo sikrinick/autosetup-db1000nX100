efb170f04602c58262149b4720ef092d64988302
Mykyta Sikriier <sikrinick@gmail.com>
2022-05-23 17:23:00 +0200

    Fix sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 cd db1000nX100/source-code/docker/db1000nX100-for-docker/ || exit 1
 cd for-macOS-and-Linux-hosts/ || exit 1
+chmod +x update-and-run.bash
 chmod +x run.bash
-./run.bash
+./update-and-run.bash
--- setup.sh
+++ setup.sh
@@ -41,5 +41,3 @@ cd .. || exit 1
 cd for-macOS-and-Linux-hosts/ || exit 1
 chmod +x update-and-run.bash
 chmod +x run.bash
-
-./update-and-run.bash
